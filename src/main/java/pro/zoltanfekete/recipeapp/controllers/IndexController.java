d9229bf45a10f0dbd8bebe49a34c114f3f6c902c
Zoltan <fekete.zoltan@live.com>
2019-12-05 11:02:36 +0100

    Initial Commit

---
+++ src/main/java/pro/zoltanfekete/recipeapp/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package pro.zoltanfekete.recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"","/","index","index.html"})
+    public String index(Model model){
+        return "index";
+    }
+}
