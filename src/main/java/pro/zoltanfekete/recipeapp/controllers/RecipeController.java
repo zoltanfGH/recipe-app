cb7403c0fc7ffbd3235fed0773e9a05ed0ceeea8
Zoltan <fekete.zoltan@live.com>
2020-01-09 15:15:41 +0100

    Added recipeform.html and updated RecipeController

--- src/main/java/pro/zoltanfekete/recipeapp/controllers/RecipeController.java
+++ src/main/java/pro/zoltanfekete/recipeapp/controllers/RecipeController.java
@@ -3,7 +3,10 @@ package pro.zoltanfekete.recipeapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import pro.zoltanfekete.recipeapp.commands.RecipeCommand;
 import pro.zoltanfekete.recipeapp.services.RecipeService;
 
 @Controller
@@ -18,5 +21,19 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @GetMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
